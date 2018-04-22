5848566b5a15ec96db2ed8b0834526d1c57c2c32
renb <91renb@gmail.com>
2018-04-22 20:34:35 +0800

    更新代码

--- BRKit/Foundation/NSArray+BRAdd.h
+++ BRKit/Foundation/NSArray+BRAdd.h
@@ -11,6 +11,16 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSArray (BRAdd)
+/** 数组/字典 转 json字符串 */
+- (nullable NSString *)br_toJsonString;
+/** 倒序数组 */
+- (NSArray *)br_reverseArray;
+
+@end
+
+@interface NSMutableArray (BRAdd)
+/** 倒序数组 */
+- (void)br_reverseMutableArray;
 
 @end
 
--- BRKit/Foundation/NSArray+BRAdd.m
+++ BRKit/Foundation/NSArray+BRAdd.m
@@ -13,4 +13,38 @@ BRSYNTH_DUMMY_CLASS(NSArray_BRAdd)
 
 @implementation NSArray (BRAdd)
 
+#pragma mark - 数组 转 json字符串
+- (NSString *)br_toJsonString {
+    if ([NSJSONSerialization isValidJSONObject:self]) {
+        NSError *error = nil;
+        // 1.转化为 JSON 格式的 NSData
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:&error];
+        if (error) {
+            NSLog(@"数组转JSON字符串失败：%@", error);
+        }
+        // 2.转化为 JSON 格式的 NSString
+        return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    }
+    return nil;
+}
+
+#pragma mark - 倒序数组
+- (NSArray *)br_reverseArray {
+    return [[self reverseObjectEnumerator] allObjects];
+}
+
+@end
+
+
+@implementation NSMutableArray (BRAdd)
+
+#pragma mark - 倒序数组
+- (void)br_reverseMutableArray {
+    NSUInteger count = self.count;
+    int mid = floor(count / 2.0);
+    for (NSUInteger i = 0; i < mid; i++) {
+        [self exchangeObjectAtIndex:i withObjectAtIndex:(count - (i + 1))];
+    }
+}
+
 @end
