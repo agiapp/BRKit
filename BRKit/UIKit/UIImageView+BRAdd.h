3f73e1456788caf335e2649661f5bb0d17172730
renb <91renb@gmail.com>
2018-05-02 11:14:49 +0800

    添加UIImageView+BRAdd分类

---
+++ BRKit/UIKit/UIImageView+BRAdd.h
@@ -0,0 +1,19 @@
+//
+//  UIImageView+BRAdd.h
+//  BRKitDemo
+//
+//  Created by 任波 on 2018/5/2.
+//  Copyright © 2018年 91renb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (BRAdd)
+
+/** 使用 CAShapeLayer 和 UIBezierPath 设置圆角 */
+- (void)br_setBezierPathCornerRadius:(CGFloat)radius;
+
+/** 通过 Graphics 和 BezierPath 设置圆角（推荐用这个）*/
+- (void)br_setGraphicsCornerRadius:(CGFloat)radius;
+
+@end
---
+++ BRKit/UIKit/UIImageView+BRAdd.m
@@ -0,0 +1,33 @@
+//
+//  UIImageView+BRAdd.m
+//  BRKitDemo
+//
+//  Created by 任波 on 2018/5/2.
+//  Copyright © 2018年 91renb. All rights reserved.
+//
+
+#import "UIImageView+BRAdd.h"
+
+@implementation UIImageView (BRAdd)
+
+#pragma mark - 使用 CAShapeLayer 和 UIBezierPath 设置圆角
+- (void)br_setBezierPathCornerRadius:(CGFloat)radius {
+    // 创建BezierPath 并设置角 和 半径 这里只设置了 左上 和 右上
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:radius];
+    CAShapeLayer *layer = [[CAShapeLayer alloc] init];
+    layer.frame = self.bounds;
+    layer.path = path.CGPath;
+    self.layer.mask = layer;
+}
+
+#pragma mark - 通过 Graphics 和 BezierPath 设置圆角（推荐用这个）
+- (void)br_setGraphicsCornerRadius:(CGFloat)radius {
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, 1.0);
+    [[UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:radius] addClip];
+    [self drawRect:self.bounds];
+    self.image = UIGraphicsGetImageFromCurrentImageContext();
+    // 结束
+    UIGraphicsEndImageContext();
+}
+
+@end
