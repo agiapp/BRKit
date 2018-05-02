ccc3de6be4b8ca18fcc74b17f6dc5b8cba5f1a54
renb <91renb@gmail.com>
2018-05-02 11:14:26 +0800

    添加UIButton+BRAdd分类

---
+++ BRKit/UIKit/UIButton+BRAdd.h
@@ -0,0 +1,34 @@
+//
+//  UIButton+BRAdd.h
+//  BRKitDemo
+//
+//  Created by 任波 on 2018/5/2.
+//  Copyright © 2018年 91renb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSUInteger, BRButtonEdgeInsetsStyle) {
+    /** image在上，label在下 */
+    BRButtonEdgeInsetsStyleTop,
+    /** image在左，label在右 */
+    BRButtonEdgeInsetsStyleLeft,
+    /** image在下，label在上 */
+    BRButtonEdgeInsetsStyleBottom,
+    /** image在右，label在左 */
+    BRButtonEdgeInsetsStyleRight
+};
+
+@interface UIButton (BRAdd)
+
+/**
+ *  设置button的文字和图片的布局样式，及间距
+ *
+ *  @param style 文字和图片的布局样式
+ *  @param space 文字和图片的间距
+ */
+- (void)br_layoutButtonWithEdgeInsetsStyle:(BRButtonEdgeInsetsStyle)style
+                        imageTitleSpace:(CGFloat)space;
+
+@end
+
