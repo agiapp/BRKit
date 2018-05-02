674a8327d0fd5bb50708869082d468db179d029e
renb <91renb@gmail.com>
2018-05-02 17:55:28 +0800

    修改头文件

--- BRKit/BRKit.h
+++ BRKit/BRKit.h
@@ -9,8 +9,41 @@
 #ifndef BRKit_h
 #define BRKit_h
 
+#if __has_include(<BRKit/BRKit.h>)
+#import <BRKit/BRKitMacro.h>
+#import <BRKit/BRMethod.h>
+#import <BRKit/NSString+BRAdd.h>
+#import <BRKit/NSArray+BRAdd.h>
+#import <BRKit/NSDictionary+BRAdd.h>
+#import <BRKit/NSDate+BRAdd.h>
+#import <BRKit/NSNumber+BRAdd.h>
+#import <BRKit/NSTimer+BRAdd.h>
+
+#import <BRKit/UIView+BRAdd.h>
+#import <BRKit/UIImage+BRAdd.h>
+#import <BRKit/UIImageView+BRAdd.h>
+#import <BRKit/UIButton+BRAdd.h>
+#import <BRKit/UIColor+BRAdd.h>
+
+#else
+
+#import "BRKitMacro.h"
 #import "BRMethod.h"
 #import "NSString+BRAdd.h"
 #import "NSArray+BRAdd.h"
+#import "NSDictionary+BRAdd.h"
+#import "NSDate+BRAdd.h"
+#import "NSNumber+BRAdd.h"
+#import "NSTimer+BRAdd.h"
+
+#import "UIView+BRAdd.h"
+#import "UIImage+BRAdd.h"
+#import "UIImageView+BRAdd.h"
+#import "UIButton+BRAdd.h"
+#import "UIColor+BRAdd.h"
+
+#endif
+
+
 
 #endif /* BRKit_h */
