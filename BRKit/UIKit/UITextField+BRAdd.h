97ece6d428d551bb915f4dc91eeb46daed5adeb4
任波 <borenfocus@gmail.com>
2018-05-11 16:39:09 +0800

    添加UITextField+BRAdd分类，实现了控制输入长度

---
+++ BRKit/UIKit/UITextField+BRAdd.h
@@ -0,0 +1,15 @@
+//
+//  UITextField+BRAdd.h
+//  BRKitDemo
+//
+//  Created by 任波 on 2018/5/11.
+//  Copyright © 2018年 91renb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (BRAdd)
+/** UITextField 的最大输入长度 */
+@property (nonatomic, assign) NSInteger inputLimit;
+
+@end
---
+++ BRKit/UIKit/UITextField+BRAdd.m
@@ -0,0 +1,40 @@
+//
+//  UITextField+BRAdd.m
+//  BRKitDemo
+//
+//  Created by 任波 on 2018/5/11.
+//  Copyright © 2018年 91renb. All rights reserved.
+//
+
+#import "UITextField+BRAdd.h"
+#import <objc/runtime.h>
+
+const char *kInputLimitKey = "kInputLimit";
+
+@implementation UITextField (BRAdd)
+
++ (void)load {
+    // 添加监听(监听文本的变化)
+    [[NSNotificationCenter defaultCenter] addObserver:[self class] selector:@selector(textFieldTextDidChange:) name:UITextFieldTextDidChangeNotification object:nil];
+}
+
+#pragma mark - 通知事件
+- (void)textFieldTextDidChange:(NSNotification *)notification {
+    UITextField *textField = (UITextField *)notification.object;
+    if (self.inputLimit > 0 && textField.text.length > self.inputLimit && textField.markedTextRange == nil) {
+        textField.text = [textField.text substringFromIndex:self.inputLimit - 1];
+    }
+}
+
+#pragma mark - setter方法
+- (void)setInputLimit:(NSInteger)inputLimit {
+    objc_setAssociatedObject(self, kInputLimitKey, @(inputLimit), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+#pragma mark - getter方法
+- (NSInteger)inputLimit {
+    NSNumber *limit = objc_getAssociatedObject(self, kInputLimitKey);
+    return [limit integerValue];
+}
+
+@end
