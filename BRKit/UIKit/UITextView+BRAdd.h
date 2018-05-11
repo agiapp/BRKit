e4893a0d71bea356ae890704914c00a894eb3ffa
任波 <borenfocus@gmail.com>
2018-05-11 16:40:09 +0800

    添加UITextView+BRAdd分类，实现了控制输入长度

---
+++ BRKit/UIKit/UITextView+BRAdd.h
@@ -0,0 +1,15 @@
+//
+//  UITextView+BRAdd.h
+//  BRKitDemo
+//
+//  Created by 任波 on 2018/5/11.
+//  Copyright © 2018年 91renb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (BRAdd)
+/** UITextView 的最大输入长度 */
+@property (nonatomic, assign) NSInteger inputLimit;
+
+@end
---
+++ BRKit/UIKit/UITextView+BRAdd.m
@@ -0,0 +1,40 @@
+//
+//  UITextView+BRAdd.m
+//  BRKitDemo
+//
+//  Created by 任波 on 2018/5/11.
+//  Copyright © 2018年 91renb. All rights reserved.
+//
+
+#import "UITextView+BRAdd.h"
+#import <objc/runtime.h>
+
+const char *kInputLimitKey = "kInputLimit";
+
+@implementation UITextView (BRAdd)
+
++ (void)load {
+    // 添加监听(监听文本的变化)
+    [[NSNotificationCenter defaultCenter] addObserver:[self class] selector:@selector(textViewTextDidChange:) name:UITextViewTextDidChangeNotification object:nil];
+}
+
+#pragma mark - 通知事件
+- (void)textViewTextDidChange:(NSNotification *)notification {
+    UITextView *textView = (UITextView *)notification.object;
+    if (self.inputLimit > 0 && textView.text.length > self.inputLimit && textView.markedTextRange == nil) {
+        textView.text = [textView.text substringFromIndex:self.inputLimit - 1];
+    }
+}
+
+#pragma mark - setter方法
+- (void)setInputLimit:(NSInteger)inputLimit {
+    objc_setAssociatedObject(self, kInputLimitKey, @(inputLimit), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+#pragma mark - getter方法
+- (NSInteger)inputLimit {
+    NSNumber *limit = objc_getAssociatedObject(self, kInputLimitKey);
+    return [limit integerValue];
+}
+
+@end
