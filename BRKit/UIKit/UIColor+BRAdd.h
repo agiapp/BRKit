3918850bfa3fd06d5ac64cf757ed6a06c76b19f9
任波 <borenfocus@gmail.com>
2018-05-19 23:59:59 +0800

    aa

--- BRKit/UIKit/UIColor+BRAdd.h
+++ BRKit/UIKit/UIColor+BRAdd.h
@@ -44,6 +44,17 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (nullable UIColor *)br_colorWithHexString:(NSString *)hexStr;
 
+/**
+ *  @brief  渐变颜色
+ *
+ *  @param fromColor  开始颜色
+ *  @param toColor    结束颜色
+ *  @param height     渐变高度
+ *
+ *  @return 渐变颜色
+ */
++ (UIColor*)br_gradientFromColor:(UIColor*)fromColor toColor:(UIColor*)toColor withHeight:(CGFloat)height;
+
 @end
 
 NS_ASSUME_NONNULL_END
