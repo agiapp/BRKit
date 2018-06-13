d54a1908612818dd9b122e32a5f6114a39bbe125
任波 <borenfocus@gmail.com>
2018-06-13 22:50:46 +0800

    更新

--- BRKit/BRKit.h
+++ BRKit/BRKit.h
@@ -27,6 +27,7 @@
 #import <BRKit/UIControl+BRAdd.h>
 #import <BRKit/UITextField+BRAdd.h>
 #import <BRKit/UITextView+BRAdd.h>
+#import <BRKit/UIDevice+BRAdd.h>
 
 #else
 
@@ -47,6 +48,7 @@
 #import "UIControl+BRAdd.h"
 #import "UITextField+BRAdd.h"
 #import "UITextView+BRAdd.h"
+#import "UIDevice+BRAdd.h"
 
 #endif
 
